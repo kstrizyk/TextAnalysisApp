b0c20f0da283144d725f5a59c156e784d53836b4
Aleksa-Strak <aleksa.strak@gmail.com>
2022-05-26 00:15:04 +0200

    Update example.R

--- src/cleaning_data/example.R
+++ src/cleaning_data/example.R
@@ -7,7 +7,7 @@ library(jsonlite)
 
 files = list.files(path)
 
-
+#FUNCTION TURNING JSON FILE TO TABLE
 JSON_to_table<- function(files, i){
   full_path = paste(path, files[i], sep = "/")
   dt = jsonlite::fromJSON(full_path)
@@ -15,12 +15,16 @@ JSON_to_table<- function(files, i){
   return(dt_table)
 }
 
+#TABLE OF ALL FILES
 dt_long = data.table()
 
+#LOOP CREATING ONE LARGE TABLE FO ALL FILES
 for (i in 1:length(files)){
  name = paste0("table", i) # czy potrzebujemy pojedynczych data.table dla kazdego pliku?
  assign(name, JSON_to_table(files, i))
  dt_long = bind_rows(dt_long, JSON_to_table(files, i))
 }
 
-
+#WE NEED TO ADD 2 EXTRA COLUMNS FOR EACH USER IN USERS.CSV
+#COLUMNS SHOULD CONTAIN INFO ABOUT LAST AND FIRST TWEET FOR EACH USER
+  #PROBLEM: IN DATA.TABLE WE HAVE USER.ID, IN CSV FILE WE HAVE USER.NAME
