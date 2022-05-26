1b356743ddf0f8466929fa498d4847e3ebec0b47
Aleksa-Strak <aleksa.strak@gmail.com>
2022-05-26 14:59:17 +0200

    Update example.R

--- src/cleaning_data/example.R
+++ src/cleaning_data/example.R
@@ -28,10 +28,11 @@ tableOfAllTweets = rbindlist(listOfAll, use.names = TRUE)
 
 # CHANGE COLUMN NAMES IN DATA.TABLE OF ALL TWEETS 
 # FOR MORE INTUITIVE APPROACH
-colNamesNew =  c("creationDate", "userId", 
+colNamesNewTweets =  c("creationDate", "userId", 
                  "text", "tweetId", "retweetCount", 
                  "replyCount", "likeCount", "quoteCount")
-setnames(tableOfAllTweets, colNamesNew)
+setnames(tableOfAllTweets, colNamesNewTweets)
+tableOfAllTweets[, userId:=as.double(userId)] #CHANGE COLUMN TYPE TO BE THE SAME AS IN USERS.CSV
 
 
 # 2. CREATE CSV FILE FOR ALL TWEETS
@@ -42,4 +43,31 @@ write.csv(tableOfAllTweets, paste(path1, "/tweets.csv", sep=""), row.names = FAL
 # 3. UPDATE USERS.CSV, 
 #    ADDING FIRST AND LAST TWEET ID AND DATE FOR EACH USER,
 #    ALSO CHANGE COLUMN NAMES IN THIS FILE
+
+# READING USERS.CSV AS DATA.TABLE
+# AND CHANGING COLUMN NAMES
+tableOfAllUsers = as.data.table(read.csv(paste(path1, "/users.csv", sep="")))
+colNamesNewUsers =  c("name", "fullName", 
+                 "description", "userId", "verification", 
+                 "followersCount", "followingCount", "tweetCount",
+                 "listedCount", "lastUpdate")
+setnames(tableOfAllUsers, colNamesNewUsers)
+
+# FIRST AND LAST TWEET AND DATES OF THESE TWEETS - NEW COLUMNS
+
+#NUMBER OF USER IN TWEETS.CSV AND USERS.CSV IS NOT EQUAL
+users = unique(tableOfAllTweets$userId) 
+users1 = unique(tableOfAllUsers$userId) #MORE USERS THAN IN TWEETS, BY 7
+
+findCrucialTweets <- function(table,id){
+  tableForUser = table[userId == id]
+  tableForUser = tableForUser[order(as.Date(tableForUser$creationDate, 
+                                            format = "%Y/%m/%d")),]
+  firstTweetDate = tableForUser$creationDate[1]
+  lastTweetDate = tail(tableForUser$creationDate, n=1)
+  firstTweetId = tableForUser$tweetId[1]
+  lastTweetId = tail(tableForUser$tweetId, n= 1)
+}
+
+#tableOfAllUsers[, `:=` ("firstTweet"=0, "firstTweetDate"=0, "lastTweet"=0, "lastTweetDate"=0)]
 
