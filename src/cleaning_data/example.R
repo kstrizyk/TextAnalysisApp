7ab022a06bb9f2c89d8580c3f983ad3dc9dfa623
Aleksa-Strak <aleksa.strak@gmail.com>
2022-05-26 12:27:56 +0200

    Update example.R

--- src/cleaning_data/example.R
+++ src/cleaning_data/example.R
@@ -1,27 +1,34 @@
+#GET PATH TO JSON FILES
 path = config::get('path-dir')[['data-twitter']]
 
+#ACTIVATE NEEDED LIBRARIES
 library(data.table)
 library(jsonlite)
 
 
-#  HOW TO READ DATA1 TO DATATABLE?
+# 1. HOW TO READ DATA1 TO DATATABLE?
 
+#CREATE FILE LIST
 files = list.files(path)
 
 #FUNCTION TURNING JSON FILE TO TABLE
+#SINGLE ARGUMENT: NAME 0F THE JSON FILE
+#RETURNS CLASS DATA.TABLE
 JSON_to_table<- function(file_name){
   full_path = paste(path, file_name, sep = "/")
   dt = fromJSON(full_path)
-  dt_table = as.data.table(dt$data)
+  dt_table = as.data.table(dt$data) #JUST DATA, WE DO NOT USE META
   return(dt_table)
 }
 
+#CREATE LIST OF DATA.TABLES OF ALL FILES IN FILE LIST
 listOfAll = lapply(files, JSON_to_table)
+#MERGE ALL DATA.TABLES FROM LIST TO ONE DATA.TABLE
 tableOfAll = rbindlist(listOfAll, use.names = TRUE)
 
 
+# 2. CREATE CSV FILE FOR ALL TWEETS
 
-#WE NEED TO ADD 2 EXTRA COLUMNS FOR EACH USER IN USERS.CSV
-#COLUMNS SHOULD CONTAIN INFO ABOUT LAST AND FIRST TWEET FOR EACH USER
-  #PROBLEM: IN DATA.TABLE WE HAVE USER.ID, IN CSV FILE WE HAVE USER.NAME
+# 3. UPDATE USERS.CSV, 
+#    ADDING FIRST AND LAST TWEET ID AND DATE FOR EACH USER
 
