3e10e827370b74d70bbae41d492c310f0de7db04
Aleksa-Strak <aleksa.strak@gmail.com>
2022-05-26 12:24:15 +0200

    Update example.R

--- src/cleaning_data/example.R
+++ src/cleaning_data/example.R
@@ -2,30 +2,26 @@ path = config::get('path-dir')[['data-twitter']]
 
 library(data.table)
 library(jsonlite)
-library(dplyr) #IS IT IN SETUP??
+
 
 #  HOW TO READ DATA1 TO DATATABLE?
 
 files = list.files(path)
 
 #FUNCTION TURNING JSON FILE TO TABLE
-JSON_to_table<- function(files, i){
-  full_path = paste(path, files[i], sep = "/")
-  dt = jsonlite::fromJSON(full_path)
-  dt_table = data.table::as.data.table(dt$data)
+JSON_to_table<- function(file_name){
+  full_path = paste(path, file_name, sep = "/")
+  dt = fromJSON(full_path)
+  dt_table = as.data.table(dt$data)
   return(dt_table)
 }
 
-#TABLE OF ALL FILES
-dt_long = data.table()
+listOfAll = lapply(files, JSON_to_table)
+tableOfAll = rbindlist(listOfAll, use.names = TRUE)
 
-#LOOP CREATING ONE LARGE TABLE FO ALL FILES
-for (i in 1:length(files)){
- name = paste0("table", i) # czy potrzebujemy pojedynczych data.table dla kazdego pliku?
- assign(name, JSON_to_table(files, i))
- dt_long = bind_rows(dt_long, JSON_to_table(files, i))
-}
+
 
 #WE NEED TO ADD 2 EXTRA COLUMNS FOR EACH USER IN USERS.CSV
 #COLUMNS SHOULD CONTAIN INFO ABOUT LAST AND FIRST TWEET FOR EACH USER
   #PROBLEM: IN DATA.TABLE WE HAVE USER.ID, IN CSV FILE WE HAVE USER.NAME
+
