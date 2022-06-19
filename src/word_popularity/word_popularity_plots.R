66b7bb7ee115e5641d6ce17d0afedc11b328c65a
Aleksa-Strak <aleksa.strak@gmail.com>
2022-06-19 19:56:55 +0200

    word popularity daily in tweets
    1. created function summing word occurence in tweets during each day
    2. words are from vector "words" given in file word_popularity_plot.R, which is changeable without any harm to code
    3. used ggplot2 to create function showing word popularity in tweets daily (word is a variable for this function)

---
+++ src/word_popularity/word_popularity_plots.R
@@ -0,0 +1,45 @@
+library(data.table)
+library(ggplot2)
+library(DT)
+library(dplyr)
+library(forecast)
+
+#reading data from file with cleaned tweets
+path = config::get('path-dir')[['data-clean']]
+tableOfAllTweets = fread(paste(path, "/tweets.csv", sep=""))
+
+words = c("war", "putin", "zelensky", "ukraine", "russia", 
+          "bucha","kharkiv", "mariupol", "war crimes")
+
+# function searching word popularity over time (per date)
+word_occurrence = function(words){
+  tableOfWords = tableOfAllTweets[, .(date)]
+  tableOfWords[,date:= format(as.POSIXct(date,format='%Y-%m-%d %H:%M:%S'),format='%Y-%m-%d')]
+  for (word in words){
+    tableOfWords[, (word):=as.integer(grepl(word, tableOfAllTweets$tweetContent))]
+    tableOfWords[, (word):=sum(get(word)), by=date]
+  }
+  unique(tableOfWords)
+}
+tableOfWords = word_occurrence(words)
+tableOfWords =tableOfWords[, date := as.Date(date)]
+
+# function drawing plot
+
+word_popularity_plot = function(word){
+  ggplot(tableOfWords)+geom_point(aes(x=date,y=get(word)))+labs(x="months of 2022", 
+                      y="number of tweets containing this word", title = "Plot showing word popularity in tweets daily", 
+                      subtitle = paste("selected word:", word))+theme(plot.title = 
+                      element_text(hjust = 0.5), plot.subtitle = 
+                      element_text(hjust = 0.5, color ="navy"),
+                      panel.background = element_rect( fill="white",
+                      colour='black'), panel.grid.major = 
+                      element_line(colour = "black"),)+scale_x_date(date_labels = "%m")
+}
+
+
+
+
+
+
+
